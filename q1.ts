2b82df07bb93c6e457d954c583445e31e251a5d5
Poon <zeepengineer@gmail.com>
2024-07-10 19:09:58 +0700

    Complete(?)

--- q1.ts
+++ q1.ts
@@ -1,10 +1,19 @@
+// ปุญณพัฒน์ ภาณุมนต์วาที 640610692
+
 // const axios = require("axios");
 import axios from "axios";
 
 /* assign interface/type to the function definition properly */
 const getUser = async (userId: number) => {
   /* Your code here */
-};
+  try {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    const User = response.data;
+    return User.name;
+  } catch (error) {
+    return 'INVALID USER ID';
+  }
+}
 
 //test case
 const input1 = 1;
--- q2.ts
+++ q2.ts
@@ -1,10 +1,29 @@
+// ปุญณพัฒน์ ภาณุมนต์วาที 640610692
+
 // const axios = require("axios");
 import axios from "axios";
 
 /* assign interface/type to the function definition properly */
 const getTodo = async (todoId: number) => {
   /* Your code here */
-};
+  try{
+    const ResponseTodo = await axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+    const todo = ResponseTodo.data;
+    const userId = todo.userId
+
+    const ResponseUser = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    const User = ResponseUser.data;
+
+    return {
+      owner: User.name,
+      title: todo.title,
+      completed: todo.completed
+    };
+  }  
+  catch (error) {
+    return 'INVALID TODO ID';
+  }
+  };
 
 //test case
 const input1 = 15;
